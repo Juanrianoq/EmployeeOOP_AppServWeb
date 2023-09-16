a0c24f87de8a1e9213b48b9acdad39ac66c248ea
Juan Ángel <ursk445@gmail.com>
2023-09-16 17:50:30 -0500

    Date class update

--- EmployeeOOP/EmployeeOOP/Classes/Date.cs
+++ EmployeeOOP/EmployeeOOP/Classes/Date.cs
@@ -1,4 +1,7 @@
-﻿namespace EmployeeOOP.Classes
+﻿using EmployeeOOP.Exceptions;
+using System;
+
+namespace EmployeeOOP.Classes
 {
       public class Date
       {
@@ -8,21 +11,74 @@
 	    private int _month;
 	    private int _day;
 
-	    #endregion
+            #endregion
 
-	    #region "Methods"
+            #region "Methods"
 
-	    public Date(int day, int month, int year)
-	    {
-		  _day = day;
-		  _month = month;
-		  _year = year;
-	    }
+            public Date(int day, int month, int year)
+            {
+                  _day = ValidateDay(day, month, year);
+                  _month = ValidateMonth(month);
+                  _year = ValidateYear(year);
+            }
             public override string ToString()
             {
 		  // Dia/Mes/Año
-		  var dateConcatenated = $"{_day}/{_month}/{_year}";
+		  var dateConcatenated = $"{_day:00}/{_month:00}/{_year:0000}";
 		  return dateConcatenated;
+            }
+
+            #endregion
+
+            #region "Validations"
+
+            private int ValidateYear(int year)
+            {
+                  if (year >= 1900)
+                        return year;
+                  else
+                        throw new YearException(String.Format("El año {0} no es valido", year));
+            }
+            private int ValidateMonth(int month)
+            {
+                  if (month >= 0 && month <= 12)
+                        return month;
+                  else
+                        throw new MonthException(String.Format("El mes {0} no es valido", month));
+            }
+            private int ValidateDay(int day, int month, int year)
+            {
+                  if (month == 2 && day == 29 && IsLeapYear(year))
+                  {
+                        return day;
+                  }
+                  
+                  if(month == 2 && day == 29 && IsLeapYear(year))
+                  {
+                        bool IsLeapYear = false;
+                        IsLeapYearException(IsLeapYear, year);
+                  }
+
+                  int[] daysPerMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+                  if (day >= 1 && day <= daysPerMonth[month])
+                  {
+                        return day;
+                  }
+                  else
+                  {
+                        throw new DayException(String.Format("El dia {0} no es valido para el mes {1}.", day, month));
+                  }         
+            }
+            private void IsLeapYearException(bool IsLeapYear, int year)
+            {
+                  if (!IsLeapYear)
+                  {
+                        throw new YearException(String.Format("El año {0} no es bisiesto.", year));
+                  }
+            }
+            private bool IsLeapYear(int year)
+            {
+                  return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
             }
 
             #endregion
--- EmployeeOOP/EmployeeOOP/Program.cs
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -1,9 +1,29 @@
 ﻿using EmployeeOOP.Classes;
+try
+{
+      Console.WriteLine("OOP APLICATION");
+      Console.WriteLine("---------------");
 
-Console.WriteLine("OOP APLICATION");
-Console.WriteLine("---------------");
+      // Variables.
+      int day, month, year;
 
-// Inicializacion de la clase "Date".
-Date objDate = new Date(23,5,2004);
-Console.WriteLine(objDate.ToString());
+      Console.Write("Ingresar el dia: ");
+      day = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Ingresar el mes: ");
+      month = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Ingresar el año: ");
+      year = Convert.ToInt32(Console.ReadLine());
+
+      // Inicializacion de la clase "Date".
+      Date objDate = new Date(day, month, year);
+      Console.WriteLine("\n");
+      Console.WriteLine(objDate.ToString());
+}
+catch (Exception ex)
+{
+      Console.WriteLine(ex.Message);
+}
+
 
