01e620203510bd0ab3bf5e095b77645cdc3ecd41
Juan Ángel <ursk445@gmail.com>
2023-09-16 16:58:08 -0500

    Date class created

---
+++ EmployeeOOP/EmployeeOOP/Classes/Date.cs
@@ -0,0 +1,31 @@
+﻿namespace EmployeeOOP.Classes
+{
+      public class Date
+      {
+	    #region "Fields"
+
+	    private int _year;
+	    private int _month;
+	    private int _day;
+
+	    #endregion
+
+	    #region "Methods"
+
+	    public Date(int day, int month, int year)
+	    {
+		  _day = day;
+		  _month = month;
+		  _year = year;
+	    }
+            public override string ToString()
+            {
+		  // Dia/Mes/Año
+		  var dateConcatenated = $"{_day}/{_month}/{_year}";
+		  return dateConcatenated;
+            }
+
+            #endregion
+
+      }
+}
--- EmployeeOOP/EmployeeOOP/Program.cs
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeeOOP.Classes;
+
+Console.WriteLine("OOP APLICATION");
+Console.WriteLine("---------------");
+
+// Inicializacion de la clase "Date".
+Date objDate = new Date(23,5,2004);
+Console.WriteLine(objDate.ToString());
+
