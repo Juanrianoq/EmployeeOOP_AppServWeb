7f4c3e597b9d8abe56a9f01541419f199e060756
Juan Ángel <ursk445@gmail.com>
2023-09-16 22:00:43 -0500

    New class = "CommissionEmployee"

---
+++ EmployeeOOP/EmployeeOOP/Classes/CommissionEmployee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Classes
+{
+      public class CommissionEmployee : Employee
+      {
+
+            #region "Properties"
+
+            public int CommisionPercentaje {  get; set; }
+            public decimal Sales { get; set; }
+
+            #endregion
+
+            #region "Methods"
+
+            public override decimal GetValueToPay()
+            {
+                  return Convert.ToDecimal(Sales * CommisionPercentaje);
+            }
+            public override string ToString()
+            {
+                  return $"{base.ToString()}" +
+                        $"Comision por ventas: {GetValueToPay:C2} \n\t";
+            }
+
+            #endregion
+
+      }
+}
